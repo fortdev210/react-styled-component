caa3ee6303e88bb46adfcf4280d1118836af59d8
fortdev210 <sharp.blader2050@gmail.com>
2021-01-12 04:56:02 +0800

    complete

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,63 +1,88 @@
-import React, { useState } from 'react'
-import { FaTimes, FaBars } from 'react-icons/fa';
-import { IconContext } from 'react-icons/lib';
-import { 
-    Nav, 
-    NavbarContainer,
-    NavLogo,
-    NavIcon,
-    MobileIcon,
-    NavMenu,
-    NavItem,
-    NavLinks
- } from './Navbar.elements'
+import React, { useState, useEffect } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+import { Button } from "../../globalStyles";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  NavIcon,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavItemBtn,
+  NavLinks,
+  NavBtnLink,
+} from "./Navbar.elements";
 
-const Navbar = () => {
-    const [click, setClick] = useState(false);
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    const handleClick = () => setClick(!click);
-    return (
-        <>
-        <IconContext.Provider value={{ color: '#fff' }}>
-          <Nav>
-              <NavbarContainer>
-                <NavLogo to="/">
-                    <NavIcon />
-                    ULTRA
-                </NavLogo>
-                <MobileIcon
-                    onClick={handleClick}
-                >
-                    {click ? <FaTimes /> : <FaBars />}
-                </MobileIcon>
-                <NavMenu onClick={handleClick}>
-                    <NavItem>
-                        <NavLinks to='/'>
-                            Home
-                        </NavLinks>
-                    </NavItem>
-                    <NavItem>
-                        <NavLinks to='/services'>
-                            Services
-                        </NavLinks>
-                    </NavItem>
-                    <NavItem>
-                        <NavLinks to='/products'>
-                            Products
-                        </NavLinks>
-                    </NavItem>
-                    <NavItem>
-                        <NavLinks to='/'>
-                            Home
-                        </NavLinks>
-                    </NavItem>
-                </NavMenu>
-              </NavbarContainer>
-          </Nav>  
-          </IconContext.Provider>
-        </>
-    )
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav>
+          <NavbarContainer>
+            <NavLogo to="/" onClick={closeMobileMenu}>
+              <NavIcon />
+              JIN
+            </NavLogo>
+            <MobileIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+            <NavMenu onClick={handleClick} click={click}>
+              <NavItem>
+                <NavLinks to="/" onClick={closeMobileMenu}>
+                  Home
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/services" onClick={closeMobileMenu}>
+                  Services
+                </NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/products" onClick={closeMobileMenu}>
+                  Products
+                </NavLinks>
+              </NavItem>
+              <NavItemBtn>
+                {button ? (
+                  <NavBtnLink to="/sign-up">
+                    <Button primary>SIGN UP</Button>
+                  </NavBtnLink>
+                ) : (
+                  <NavBtnLink to="/sign-up">
+                    <Button onClick={closeMobileMenu} fontBig primary>
+                      SIGN UP
+                    </Button>
+                  </NavBtnLink>
+                )}
+              </NavItemBtn>
+            </NavMenu>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
