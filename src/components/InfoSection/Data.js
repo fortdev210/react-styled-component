5345616f3ccc3b88eb4ea79e656aa89eb32305bd
fortdev210 <sharp.blader2050@gmail.com>
2021-01-14 15:16:06 +0800

    add infosection with grid layout

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transaction with zero fees",
+  description:
+    "Get access to our exclusive app that allows you send unlimited transaction",
+  buttonLabel: "Get Started",
+  imgStart: false,
+  img: require("../../images/svg-1.svg"),
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
